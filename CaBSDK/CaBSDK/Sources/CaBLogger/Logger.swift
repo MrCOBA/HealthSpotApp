1d00c79f336249b8ec0be662fec2b54eec42170d
Oleg Oparin <oa_oparin@mail.ru>
2022-04-17 13:53:57 +0300

    [HealthSporApp][CaBSDK] Add logger

--- CaBSDK/CaBSDK/Sources/CaBLogger/Logger.swift
+++ CaBSDK/CaBSDK/Sources/CaBLogger/Logger.swift
@@ -1,2 +1,29 @@
+import OSLog
 
+// MARK: - System Logger Instance
 
+private let logger = Logger()
+
+// MARK: - Default Log
+
+public func log(message: String) {
+    logger.log(message)
+}
+
+// MARK: - Info Log
+
+public func logInfo(message: String) {
+    logger.info("<INFO> \(message)")
+}
+
+// MARK: - Error Log
+
+public func logError(message: String) {
+    logger.error("<ERROR> \(message)")
+}
+
+// MARK: - Warning Log
+
+public func logWarning(message: String) {
+    logger.warning("<WARNING> \(message)")
+}
