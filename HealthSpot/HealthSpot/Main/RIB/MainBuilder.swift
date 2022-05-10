66532a23af7520e83f9fe267ca3ce71d0f927ab0
Oparin Oleg <oleg.oparin@kaspersky.com>
2022-05-10 16:28:11 +0300

    [HealthSpotApp] Prepare to bar-code scanner integration

--- HealthSpot/HealthSpot/Main/RIB/MainBuilder.swift
+++ HealthSpot/HealthSpot/Main/RIB/MainBuilder.swift
@@ -6,7 +6,10 @@ final class MainBuilder: Builder {
         let view = MainView()
 
         let interactor = MainInteractorImpl()
+        view.eventsHandler = interactor
+
         let router = MainRouterImpl(view: view, interactor: interactor)
+        interactor.router = router
 
         return router
     }
