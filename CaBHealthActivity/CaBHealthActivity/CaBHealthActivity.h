f4f2e23238ffe09092496fb1ee7be0cfe8cdf9f1
Oparin Oleg <oleg.oparin@kaspersky.com>
2022-05-23 20:24:22 +0300

    [HealthSpotApp][CaBHealthActivity] Init framework

---
+++ CaBHealthActivity/CaBHealthActivity/CaBHealthActivity.h
@@ -0,0 +1,18 @@
+//
+//  CaBHealthActivity.h
+//  CaBHealthActivity
+//
+//  Created by Oparin on 23.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CaBHealthActivity.
+FOUNDATION_EXPORT double CaBHealthActivityVersionNumber;
+
+//! Project version string for CaBHealthActivity.
+FOUNDATION_EXPORT const unsigned char CaBHealthActivityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CaBHealthActivity/PublicHeader.h>
+
+
